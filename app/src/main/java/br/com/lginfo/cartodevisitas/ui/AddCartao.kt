d797e5c8dac040c152a6f29a95d9dac053d60e81
Leonardo Gonçalves <leonardo@lginfo.com.br>
2021-07-24 01:27:25 -0300

    add activity de cadastro - layout e campos

--- app/src/main/java/br/com/lginfo/cartodevisitas/ui/AddCartao.kt
+++ app/src/main/java/br/com/lginfo/cartodevisitas/ui/AddCartao.kt
@@ -3,15 +3,10 @@ package br.com.lginfo.cartodevisitas.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.lginfo.cartodevisitas.R
-import br.com.lginfo.cartodevisitas.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-
-    private val bindig by lazy { ActivityMainBinding.inflate(layoutInflater) }
+class AddCartao : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(bindig.root)
-                    }
-
-
+        setContentView(R.layout.activity_add_cartao)
+    }
 }
